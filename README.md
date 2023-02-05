9e65d0f8085ee2183179da75f593ca04f5b5cca7
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-02-05 14:22:50 -0500

    create index.html, style.css and script.js add logic for fetching data

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# code_the_dream
+# Star Wars API
+
+## Running the Webpage
+
+1. Clone or download the repository.
+2. Open the `index.html` file in your browser.
+3. The page should display the data for the People and Films sections from the Star Wars API.
+
+
---
+++ script.js
@@ -0,0 +1,48 @@
+const peopleList = document.querySelector('#people-list');
+const filmsList = document.querySelector('#films-list');
+const urlPeople = 'https://swapi.dev/api/people/';
+const urlFilms = 'https://swapi.dev/api/films/';
+//create a helper function to fetch data
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getPeople() {
+  const people = await fetchData(urlPeople);
+  console.log(people);
+  people.results.forEach((person) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `<h3>${person.name}</h3> <p>Birth Year: ${person.birth_year}</p> <p>Hair Color: ${person.hair_color}</p> <p>Gender: ${person.gender}</p> <p><a href="#films">View Films</a></p>`;
+
+    card.addEventListener('click', async () => {
+      filmsList.innerHTML = '';
+      person.films.forEach(async (filmUrl) => {
+        const film = await fetchData(filmUrl);
+        const filmCard = document.createElement('div');
+        filmCard.classList.add('card');
+        filmCard.innerHTML = `<h3>${film.title}</h3> <p>Director: ${film.director}</p> <p>Producer: ${film.producer}</p> <p>Release Date: ${film.release_date}</p>`;
+        filmsList.appendChild(filmCard);
+      });
+    });
+    peopleList.appendChild(card);
+  });
+}
+
+async function getFilms() {
+  const films = await fetchData(urlFilms);
+  films.results.forEach((film) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `<h3>${film.title}</h3> <p>Director: ${film.director}</p> <p>Producer: ${film.producer}</p> <p>Release Date: ${film.release_date}</p> <p><a href="#people">View Characters</a></p>`;
+    filmsList.appendChild(card);
+  });
+}
+
+getPeople();
+getFilms();
